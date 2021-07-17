fed9cc739cef0102a3e3bba54c52036bcfe52276
Shahroze Husain <shahroze93@gmail.com>
2021-07-17 16:22:33 -0400

    Mediaquery of Navbar completed

--- crm-app/src/components/Navbar/Navbar.jsx
+++ crm-app/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
 import "./Navbar.css"
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
-function Navbar () {
+function Navbar() {
+  const [toggle, setToggle] = useState(false)
+
+  // added a function that will close the mobile navbar upon clicking one of the clicks by toggling: https://stackoverflow.com/questions/8206565/check-uncheck-checkbox-with-javascript
+  const handleClick = () => {
+    document.getElementById("nav-toggle").checked = false;
+  }
+
   return (
-    <nav className="nav-links">
-      <Link to="/" className="Alpha" ><h2>ALPHA CRM</h2></Link>
-      <div className="nav-menu">
-      <li className="pagelinks" ><Link to="/">Customers</Link></li>
-      <li className="pagelinks"><Link to="/contacts">Contacts</Link></li>
-      <li className="pagelinks"><Link to="/communication">Communications</Link></li>
-      <li className="pagelinks"><Link to="/newCustomer">Add Customer</Link></li>
-      </div>
+      <nav class="nav">
+      <input id="nav-toggle" type="checkbox" />
+      <Link to="/" className="logo" >ALPHA CRM</Link>
+      <ul class="links">
+      <li className="pagelinks"><Link to="/" onClick={handleClick} >Customers</Link></li>
+      <li className="pagelinks"><Link to="/contacts" onClick={handleClick} >Contacts</Link></li>
+      <li className="pagelinks"><Link to="/communication" onClick={handleClick} >Communications</Link></li>
+      <li className="pagelinks"><Link to="/newCustomer" onClick={handleClick} >Add Customer</Link></li>
+      </ul>
+      <label for="nav-toggle" class="icon-burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </label>
     </nav>
   )
 }
